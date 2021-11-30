16976150ab7b910c6353016399486e747fdb79ae
kyoungmin <rtong77@naver.com>
2021-11-30 09:03:37 +0900

    controller 구현

---
+++ src/main/java/com/example/todoserver/controller/TodoController.java
@@ -0,0 +1,72 @@
+package com.example.todoserver.controller;
+
+import com.example.todoserver.model.TodoEntity;
+import com.example.todoserver.model.TodoRequest;
+import com.example.todoserver.model.TodoResponse;
+import com.example.todoserver.service.TodoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin
+@AllArgsConstructor
+@RestController
+@RequestMapping("/")
+public class TodoController {
+
+    private final TodoService service;
+
+    @PostMapping
+    public ResponseEntity<TodoResponse> create(@RequestBody TodoRequest request) {
+
+        if (ObjectUtils.isEmpty((request.getTitle())))
+            return ResponseEntity.badRequest().build();
+
+        if (ObjectUtils.isEmpty(request.getOrder()))
+            request.setOrder(0L);
+
+        if (ObjectUtils.isEmpty(request.getCompleted()))
+            request.setCompleted(false);
+
+        TodoEntity result = this.service.add(request);
+        return ResponseEntity.ok(new TodoResponse(result));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TodoResponse> readOne(@PathVariable Long id) {
+
+        TodoEntity result = this.service.searchById(id);
+        return ResponseEntity.ok(new TodoResponse(result));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoResponse>> readAll() {
+
+        List<TodoEntity> list = this.service.searchAll();
+        List<TodoResponse> response = list.stream().map(TodoResponse::new)
+                                                    .collect(Collectors.toList());
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<TodoResponse> update(@PathVariable Long id, @RequestBody TodoRequest request) {
+        TodoEntity result = this.service.updateById(id, request);
+        return ResponseEntity.ok(new TodoResponse(result));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteOne(@PathVariable Long id) {
+        this.service.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteAll() {
+        this.service.deleteAll();
+        return ResponseEntity.ok().build();
+    }
+}
